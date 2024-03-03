a7ef3ba348fe2302bdf317fc7fbd91e4ffa3d6bd
Roberto Ríos <roberto@deveahub.com>
2024-03-04 00:46:52 +0100

    feat: add styles to not found page

--- src/scenes/document/not-found.scene.tsx
+++ src/scenes/document/not-found.scene.tsx
@@ -1,5 +1,11 @@
-const NotFoundScene = () => {
-  return <div>No se encuentra</div>;
-};
+import Section from "@/components/section";
+import Typography from "@/components/typography";
+
+import styles from "./not-found.module.scss";
+const NotFoundScene = () => (
+  <Section title="Not Found" className={styles["not-found"]}>
+    <Typography>Sorry, the page you are looking for does not exist.</Typography>
+  </Section>
+);
 
 export default NotFoundScene;
