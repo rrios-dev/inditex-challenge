ede85748c58811472a7f2ca77acba559a708baac
Roberto Ríos <roberto@deveahub.com>
2024-03-03 21:32:57 +0100

    feat: add content security policy

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,10 @@ const nextConfig = {
             key: "Cache-Control",
             value: "public, max-age=9999999999, must-revalidate",
           },
+          {
+            key: "Content-Security-Policy",
+            value: "script-src 'self';",
+          },
         ],
       },
     ];
