09a6de23b400684dfde47e6d3680a5781a34e550
Roberto Ríos <roberto@deveahub.com>
2024-03-03 18:53:30 +0100

    chore: add cache headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png|webp)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
