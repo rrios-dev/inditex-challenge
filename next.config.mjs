b74a8a028698ac8aae26ccdd56bbd777506c2f83
Roberto Ríos <roberto@deveahub.com>
2024-03-04 11:10:07 +0100

    chore: modify headers config

--- next.config.mjs
+++ next.config.mjs
@@ -5,16 +5,22 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/:all*(svg|jpg|png|webp)",
+        source: "/:all*(svg|jpg|png|webp|ico)",
         locale: false,
         headers: [
           {
             key: "Cache-Control",
             value: "public, max-age=9999999999, must-revalidate",
           },
+        ],
+      },
+      {
+        source: "/:all*(js|mjs|cjs|json)",
+        locale: false,
+        headers: [
           {
             key: "Content-Security-Policy",
-            value: "script-src 'self';",
+            value: "default-src 'self'; script-src 'self';",
           },
         ],
       },
